0b6c9058e40bd69dcbfd90fb5832fca715510e3f
Fernando Guzman <fguzman3@my.nl.edu>
2025-03-03 01:32:21 -0600

    Add files via upload

---
+++ EndGameModule.py
@@ -0,0 +1,8 @@
+#Fernando Guzman
+#03/03/25
+
+#End game module.py
+
+def end_game(player_name):
+    print(f"\nCongratulations, {player_name}! You have conquered the challenges and emerged victorious!")
+    print("Thank you for playing!")
---
+++ Main.py
@@ -0,0 +1,18 @@
+#Fernando Guzman
+#03/03/25
+
+#Main module that links all other ones together
+
+import time, BattleModule, ChoiceModule, StartModule, IntroModule, EndGameModule
+
+def main():
+    player_name = StartModule.start_game()
+    game = ChoiceModule
+    while game.chapter <= 5:
+        IntroModule.introduce_chapter(game.chapter)
+        game.ChoiceModule()
+        if game.chapter == 6:
+            BattleModule.battle(game, player_name)
+            break
+
+main()
