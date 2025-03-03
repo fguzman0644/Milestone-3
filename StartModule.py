e41b891c3ebbff4fea9344aa0c0ebb5a40a471d6
Fernando Guzman <fguzman3@my.nl.edu>
2025-03-02 23:27:03 -0600

    Add files via upload

---
+++ StartModule.py
@@ -0,0 +1,21 @@
+#Fernando Guzman
+#03/02/25
+
+#Start Module
+
+import time
+import battle_module
+import choice_module
+
+# Declare global variable
+player_name = ""
+
+def start_game():
+    global player_name
+    player_name = input("Welcome bounty hunter! What is your name? ")
+    print(f"Hello, {player_name}! Your adventure begins now.")
+    time.sleep(1)
+    choice_module()
+    
+if __name__ == "__main__":
+    start_game()
